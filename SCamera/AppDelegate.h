1abfd10e0fde9771aec06aa99a4fa811f014b6ea
LifengZhang <lfzhang1@chinaums.com>
2017-12-19 22:05:55 +0800

    Initial Commit

---
+++ SCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SCamera
+//
+//  Created by Lifeng Zhang on 2017/12/19.
+//  Copyright © 2017年 SCamera.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SCamera
+//
+//  Created by Lifeng Zhang on 2017/12/19.
+//  Copyright © 2017年 SCamera.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
