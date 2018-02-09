7e5bbf958448ac8655b3c9816e53711464c22ad8
LifengZhang <lfzhang1@chinaums.com>
2018-02-09 16:47:07 +0800

    fix the bug

--- SCamera/Camera/SCameraValueSwitch/SCameraISOValueScrollView.m
+++ SCamera/Camera/SCameraValueSwitch/SCameraISOValueScrollView.m
@@ -8,15 +8,36 @@
 
 #import "SCameraISOValueScrollView.h"
 
+@interface SCameraISOValueScrollView()
+
+@property (nonatomic, strong) NSArray *iSOValuesArray;
+
+@end
+
 @implementation SCameraISOValueScrollView
 
 - (instancetype)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
+        self.iSOValuesArray = @[@(25),@(32),@(40),@(64),@(80),@(100),@(125),@(160),@(200),@(250),@(320),@(400),@(500),@(640),@(800)];
         self.contentSize = CGSizeMake(1000, 50);
+        [self initValuesLabel];
     }
     
     return self;
+    
+}
+
+- (void)initValuesLabel {
+    
+    for (NSUInteger i = 0; i < self.iSOValuesArray.count; i++) {
+     
+        UILabel *label = [[UILabel alloc] initWithFrame:CGRectZero];
+        label.text = self.iSOValuesArray[i];
+        [label setTextColor:[UIColor whiteColor]];
+        [self addSubview:label];
+        
+    }
     
 }
 
